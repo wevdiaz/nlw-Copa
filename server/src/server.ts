7bbce50cf6c0cbe322f82eb123938aeb56a4c142
Weverson Diazz <63312710+wevdiaz@users.noreply.github.com>
2022-11-02 19:26:13 -0300

    criando funcionalidade de criar um novo bolão

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
 import z from "zod";
+import ShortUniqueId from "short-unique-id";
 
 import { PrismaClient } from "@prisma/client";
 
@@ -30,7 +31,17 @@ async function bootstrap() {
 
     const { title } = createPoolBody.parse(request.body);
 
-    return reply.status(201).send({ title });
+    const generate = new ShortUniqueId({ length: 6 });
+    const code = String(generate()).toUpperCase();
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code
+      }
+    })
+
+    return reply.status(201).send({ code });
   });
 
   await fastify.listen({ port: 3333, host: "0.0.0.0" });
