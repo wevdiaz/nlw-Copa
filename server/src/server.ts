d1b67f123811cc1f6cb22f3fc2c138f20b2cb4fa
Weverson Diazz <63312710+wevdiaz@users.noreply.github.com>
2022-11-01 22:47:31 -0300

    criando a rota para criação de bolão

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,7 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
+import z from "zod";
+
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient({
@@ -19,6 +21,16 @@ async function bootstrap() {
     const pools = await prisma.pool.count();
 
     return { pools }
+  });
+
+  fastify.post("/pools", async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    });
+
+    const { title } = createPoolBody.parse(request.body);
+
+    return reply.status(201).send({ title });
   });
 
   await fastify.listen({ port: 3333, host: "0.0.0.0" });
