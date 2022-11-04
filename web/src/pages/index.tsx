3384d5f90f57c22db454acd98b909364e7ac4e2a
Weverson Diazz <63312710+wevdiaz@users.noreply.github.com>
2022-11-03 21:10:14 -0300

    estilizando a aplicação

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -11,42 +11,55 @@ import iconCheckImg from "../assets/icon-check.svg";
 
 export default function Home() {  
   return (
-    <div>
+    <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center">
       <main>
         <Image src={logoImg} alt="Logo da aplicação - NLW Copa" />
 
-        <h1>
+        <h1 className="mt-14 text-white text-5xl font-bold leading-tight">
           Crie seu próprio bolão da copa e compartilhe entre amigos!
         </h1>
 
-        <div>
+        <div className="mt-10 flex items-center gap-2">
           <Image src={avataresUsersImg} alt="avatares de usuários" quality={100} />
-          <strong>
-            <span>+12.592</span> pessoas já estão usando
+          <strong className="text-gray-100 text-xl">
+            <span className="text-ignite-500">+12.592</span> pessoas já estão usando
           </strong>
         </div>
 
-        <form>
-          <input type="text" required placeholder="Qual nome do seu bolão?"/>
-          <button type="submit">Criar meu bolão</button>
+        <form className="mt-10 flex gap-2">
+          <input
+              className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm" 
+              type="text" 
+              required 
+              placeholder="Qual nome do seu bolão?"
+          />
+          <button 
+              className="bg-nlwYellow-500 px-6 py-4 rounded text-gray-900 font-bold uppercase hover:bg-nlwYellow-700 transition" 
+              type="submit"
+          >
+            Criar meu bolão
+          </button>
         </form>
 
-        <p>
+        <p className="mt-4 text-gray-300 text-sm leading-relaxed">
           Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀          
         </p>
 
-        <div>
-          <div>
+        <div className="mt-10 pt-10 border-t border-gray-600 flex items-center justify-between text-gray-100">
+          <div className="flex items-center gap-6">
             <Image src={iconCheckImg} alt="" />
-            <div>
-              <span>+2.034</span>
+            <div className="flex flex-col">
+              <span className="font-bold text-2xl">+2.034</span>
               <span>Bolões criados</span>
             </div>
           </div>
-          <div>
+
+          <div className="w-px h-10 bg-gray-600"></div>
+
+          <div className="flex items-center gap-6">
             <Image src={iconCheckImg} alt="" />
-            <div>
-              <span>+192.847</span>
+            <div className="flex flex-col">
+              <span className="font-bold text-2xl">+192.847</span>
               <span>Palpites enviados</span>
             </div>
           </div>
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -9,8 +9,23 @@ module.exports = {
         sans: "Roboto, sans-serif"
       },
 
+      backgroundImage: {
+        app: "url(/app-bg.png)"
+      },
+
       colors: {
+        ignite: {
+          500: "#129E57"
+        },
+        nlwYellow: {
+          500: "#f7dd43",
+          700: "#e5cd3d"
+        },
         gray: {
+          100: "#e1e1e6",
+          300: "#8d8d99",
+          600: "#323238",
+          800: "#202024",
           900: "#121214"
         }
       }
