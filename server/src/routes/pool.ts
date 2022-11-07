c09fcc3ad3bde5f64d8492fc29897e34044c531b
Weverson Diazz <63312710+wevdiaz@users.noreply.github.com>
2022-11-07 18:52:08 -0300

    listagem de bolões com dados de participantes

--- server/src/routes/pool.ts
+++ server/src/routes/pool.ts
@@ -102,4 +102,42 @@ export async function poolRoute(fastify: FastifyInstance) {
     return reply.status(201).send();
 
   });
+
+  fastify.get("/pools", { onRequest: [ authenticate ]}, async (request) => {
+    const pools = await prisma.pool.findMany({
+      where: {
+        participants: {
+          some: {
+            userId: request.user.sub,
+          }
+        }
+      },
+      include: {
+        _count: {
+          select: {
+            participants: true,
+          }
+        },
+        participants: {
+          select: {
+            id: true,
+
+            user: {
+              select: {
+                avatarUrl: true,
+              }
+            }
+          }
+        },
+        owner: {
+          select: {
+            name: true,
+            id: true,
+          }
+        }
+      }
+    });
+
+    return { pools }
+  });
 }
