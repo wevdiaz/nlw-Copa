c638d575ab5ad891a7b87693ffa1ac1271cca7a3
Weverson Diazz <63312710+wevdiaz@users.noreply.github.com>
2022-11-08 18:21:51 -0300

    add title e favicon

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -7,6 +7,8 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+        <title>.: NLW Copa 2022 :.</title>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </Head>
       <body className="bg-gray-900 bg-app bg-no-repeat bg-cover">
         <Main />
